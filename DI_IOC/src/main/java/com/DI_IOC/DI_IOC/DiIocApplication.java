9244e5df96d068c94762a18b35c362a28212b340
NSDeveleopers777 <neeti07121@gmail.com>
2026-01-09 13:20:49 +0530

    IoC & DI

---
+++ DI_IOC/src/main/java/com/DI_IOC/DI_IOC/DiIocApplication.java
@@ -0,0 +1,13 @@
+package com.DI_IOC.DI_IOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiIocApplication.class, args);
+	}
+
+}
---
+++ DI_IOC/src/test/java/com/DI_IOC/DI_IOC/DiIocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DI_IOC.DI_IOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiIocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
